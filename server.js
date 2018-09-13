b84693554071a5755e9a63abcc6e18cb7e0792f7
Robel Tekle <robel.tekle@gmail.com>
2018-09-13 21:36:04 +0200

    heroku config

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
+
 var app = express()
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
@@ -44,4 +46,4 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => console.log('Server is up at port 3000'))
+app.listen(port, () => console.log(`Server is up at port ${port}`))
